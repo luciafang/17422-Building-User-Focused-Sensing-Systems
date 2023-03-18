47d267f18f58da6cc4906321a1147bd4ec5eba1a
alexh <104598310+luciaFang@users.noreply.github.com>
2023-03-18 17:49:04 -0400

    audio custom components

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import os
+import numpy as np
+from io import BytesIO
+import streamlit.components.v1 as components
+from st_custom_components import st_audiorec
 
 picture = st.camera_input("Take a picture")
+wav_audio_data = st_audiorec()
 
+if wav_audio_data is not None:
+    # display audio data as received on the backend
+    st.audio(wav_audio_data, format='audio/wav')
+    
 if picture:
     st.image(picture)
