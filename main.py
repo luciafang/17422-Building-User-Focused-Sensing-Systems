b02fdb779ce19b67e85254d0f993c6f7a8a232d6
Alex <104598310+luciaFang@users.noreply.github.com>
2023-03-18 18:49:58 -0400

    new audio recorder via pip install

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import streamlit as st
-import os
-import numpy as np
-from io import BytesIO
-import streamlit.components.v1 as components
-from st_custom_components import st_audiorec
+from audio_recorder_streamlit import audio_recorder
+
 
 picture = st.camera_input("Take a picture")
-wav_audio_data = st_audiorec()
-
-if wav_audio_data is not None:
-    # display audio data as received on the backend
-    st.audio(wav_audio_data, format='audio/wav')
+audio_bytes = audio_recorder()
+if audio_bytes:
+    st.audio(audio_bytes, format="audio/wav")
 
 if picture:
     st.image(picture)
